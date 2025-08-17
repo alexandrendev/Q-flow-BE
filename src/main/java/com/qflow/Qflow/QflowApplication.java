1f2d3fa369d6e8bc1ec2ab4e5e496921be7f6959
Alexandre N <alxfn86@gmail.com>
2025-08-17 14:15:21 -0300

    chore: upload project

---
+++ src/main/java/com/qflow/Qflow/QflowApplication.java
@@ -0,0 +1,13 @@
+package com.qflow.Qflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QflowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/qflow/Qflow/QflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qflow.Qflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
